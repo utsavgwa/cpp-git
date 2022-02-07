7078d13a43e10aea8da510dd28ed18f2b79cff89
Utsav <utsavgwa@gmail.com>
2022-02-07 10:03:03 +0530

    program to reverse an array

---
+++ reverse_array.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseArray(int *input, int n);
+void printArray(int *input, int n);
+
+int main(){
+    int n;
+    cin >> n;
+
+    int *input = new int[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> input[i];
+    }
+    
+    reverseArray(input, n);
+    printArray(input, n);
+
+    
+}
+void reverseArray(int *input, int n){
+    int i=0, j=n-1;
+    while(i<j){
+        swap(input[i], input[j]);
+        i++;
+        j--;
+    }
+}
+void printArray(int *input, int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout << input[i]<< " ";
+    }   
+}
